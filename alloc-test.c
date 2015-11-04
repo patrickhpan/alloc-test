94606a0a0866f3287d34695f055879d40587a3ca
Patrick Pan <patrick.pan@patrickpan.com>
2015-11-04 04:00:28 -0500

    Moved allocation to different function to allow testing for larger number of trials and sizes.

--- alloc-test.c
+++ alloc-test.c
@@ -3,21 +3,29 @@
 #include <string.h>
 #include <time.h>
 
+void alloca_test(size_t size) {
+  void* test = alloca(size);
+  (void) test;
+}
+
+void malloc_test(size_t size) {
+  void* test = malloc(size);
+  free(test);
+}
+
 int main(int argc, char** argv) {
   if(argc != 3) return 1;
   int trials = atoi(argv[1]);
   size_t size = (size_t) atoi(argv[2]);
   clock_t alloca_start = clock();
   for(int i = 0; i < trials; i++) {
-    void* test = alloca(size);
-    (void) test;
+    alloca_test(size);
   }
   clock_t alloca_end = clock();
   printf("Alloca: %i allocations of %zu bytes took %lu clocks.\n", trials, size, alloca_end - alloca_start);
   clock_t malloc_start = clock();
   for(int i = 0; i < trials; i++) {
-    void* test = malloc(size);
-    free(test);
+    malloc_test(size);
   }
   clock_t malloc_end = clock();
   printf("Malloc: %i allocations of %zu bytes took %lu clocks.\n", trials, size, malloc_end - malloc_start);
